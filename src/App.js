b02377f4d13e6f684876738b92efe1f4bbcb0479
H21ook <hishigbayar_3@yahoo.com>
2019-06-18 11:53:07 +0800

    routing oorchlow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import  firebase from './firebase_config';
 import LoginForm from './Pages/LoginPage/Login';
+import RegisterForm from './Pages/RegisterPage/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router basename="/react-auth-ui/">
-        <div className="App">
+      <Router>
           <Route exact path="/" component={LoginForm}></Route>
-        </div>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/register" component={RegisterForm}></Route>
       </Router>
     );
   }
