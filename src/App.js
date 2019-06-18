705945558f4aae6bbdbc42e88b80a6df4a30b5db
H21ook <hishigbayar_3@yahoo.com>
2019-06-18 23:41:07 +0800

    zamchlalaar huwisagch damjuulah

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import  firebase from './firebase_config';
 import LoginForm from './Pages/LoginPage/Login';
 import RegisterForm from './Pages/RegisterPage/Register';
+import firebase from './firebaseConnect';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <Router>
           <Route exact path="/" component={LoginForm}></Route>
-          <Route path="/login" component={LoginForm}></Route>
-          <Route path="/register" component={RegisterForm}></Route>
+          <Route path="/login" component={() => <LoginForm/>}></Route>
+          <Route path="/register" component={() => <RegisterForm db={firebase}/>}></Route>
       </Router>
     );
   }
